1792db5b0af4604724ba6d1a2578ac1ea25e628f
ajwilhelm55 <46696504+ajwilhelm55@users.noreply.github.com>
2019-03-11 20:05:12 -0400

    Add files via upload

---
+++ InclassAssignment3.11.2019.py
@@ -0,0 +1,11 @@
+def maximumnumbers(integerA, integerB, integerC):
+    return max(integerA, integerB, integerC)
+
+print("This program finds the max of the numbers inputted:")
+
+integerA = int(input("Type the first number:"))
+integerB = int(input("Type the second number:"))
+integerC = int(input("Type the third number:"))
+
+
+print(maximumnumbers(integerA, integerB, integerC))
